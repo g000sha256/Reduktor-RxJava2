967e27c8d38c0dab07bb0479a9ea490c91206d76
g000sha256 <g000sha256@gmail.com>
2021-10-11 14:13:03 +0300

    .

--- source/core/src/main/kotlin/ru/g000sha256/reduktor/Store.kt
+++ source/core/src/main/kotlin/ru/g000sha256/reduktor/Store.kt
@@ -3,70 +3,79 @@ package ru.g000sha256.reduktor
 class Store<A, S>(
     initialState: S,
     private val reducer: Reducer<A, S>,
-    private val initializers: Iterable<Initializer<A, S>> = emptyList(),
+    initializers: Iterable<Initializer<A, S>> = emptyList(),
     private val sideEffects: Iterable<SideEffect<A, S>> = emptyList(),
     private val logger: Logger = Logger {},
     private val onNewState: (state: S) -> Unit
 ) {
 
-    private val actionsOwner: ActionsOwner<A>
+    private val actionsOwner = createActionsOwner()
     private val lock = Any()
 
     private var state = initialState
 
     init {
-        val actions = object : Actions<A> {
+        logger.apply {
+            invoke("STATE  : $initialState")
+            logThread()
+        }
+        onNewState(initialState)
+        initializers.forEach { it.apply { actionsOwner.invoke(initialState) } }
+    }
+
+    private fun createActions(): Actions<A> {
+        return object : Actions<A> {
 
             override fun post(action: A) {
-                handleAction(action)
+                synchronized(lock) { handleAction(action) }
             }
 
             override fun post(vararg actions: A) {
-                actions.forEach(::handleAction)
+                synchronized(lock) { actions.forEach(::handleAction) }
             }
 
             override fun post(actions: Iterable<A>) {
-                actions.forEach(::handleAction)
+                synchronized(lock) { actions.forEach(::handleAction) }
             }
 
         }
-        actionsOwner = ActionsOwnerImpl(actions)
-        logger.apply {
-            invoke("STATE  : $state")
-            logThread()
-        }
-        synchronized(lock) {
-            val state = state
-            initializers.forEach { it.apply { actionsOwner.invoke(state) } }
+    }
+
+    private fun createActionsOwner(): ActionsOwner<A> {
+        return object : ActionsOwner<A> {
+
+            override val actions = createActions()
+
         }
     }
 
     private fun handleAction(action: A) {
-        synchronized(lock) {
-            val oldState = state
-            logger.apply {
-                logSeparator()
-                invoke("ACTION > $action")
-                invoke("STATE  > $oldState")
-            }
-            val newState = reducer.run { oldState.invoke(action) }
-            if (newState == oldState) {
-                logger.apply {
-                    invoke("STATE  : NOT CHANGED")
-                    logThread()
-                }
-            } else {
-                state = newState
-                logger.apply {
-                    invoke("STATE  < $newState")
-                    logThread()
-                }
-                onNewState(newState)
-            }
-            sideEffects.forEach { it.apply { actionsOwner.invoke(action, newState) } }
+        val oldState = state
+        logger.apply {
+            invoke("---------")
+            invoke("ACTION > $action")
+            invoke("STATE  > $oldState")
         }
+        val newState = reducer.run { oldState.invoke(action) }
+        if (newState == oldState) {
+            logger.apply {
+                invoke("STATE  : NOT CHANGED")
+                logThread()
+            }
+        } else {
+            state = newState
+            logger.apply {
+                invoke("STATE  < $newState")
+                logThread()
+            }
+            onNewState(newState)
+        }
+        sideEffects.forEach { it.apply { actionsOwner.invoke(action, newState) } }
     }
 
-    private class ActionsOwnerImpl<A>(override val actions: Actions<A>) : ActionsOwner<A>
+    private fun Logger.logThread() {
+        val thread = Thread.currentThread()
+        invoke("THREAD : ${thread.name}")
+    }
 
 }
