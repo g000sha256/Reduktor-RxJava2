22457b219960d5b8548bacedec7e6623c5a0c387
g000sha256 <g000sha256@gmail.com>
2021-10-13 23:02:14 +0300

    .

--- source/coroutines/src/main/kotlin/ru/g000sha256/reduktor/coroutines/TEST.kt
+++ source/coroutines/src/main/kotlin/ru/g000sha256/reduktor/coroutines/TEST.kt
@@ -3,7 +3,9 @@ package ru.g000sha256.reduktor.coroutines
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import ru.g000sha256.reduktor.core.Environment
+import ru.g000sha256.reduktor.core.plusAssign
 import ru.g000sha256.reduktor.core.post
+import ru.g000sha256.reduktor.core.set
 
 fun Environment<String>.test(coroutineScope: CoroutineScope) {
     tasks += coroutineScope.createTask(Dispatchers.IO) { actions post "..." }
--- source/rxjava3/src/main/kotlin/ru/g000sha256/reduktor/rxjava3/TEST.kt
+++ source/rxjava3/src/main/kotlin/ru/g000sha256/reduktor/rxjava3/TEST.kt
@@ -2,6 +2,8 @@ package ru.g000sha256.reduktor.rxjava3
 
 import io.reactivex.rxjava3.core.Single
 import ru.g000sha256.reduktor.core.Environment
+import ru.g000sha256.reduktor.core.plusAssign
+import ru.g000sha256.reduktor.core.set
 
 fun Environment<String>.test() {
     tasks += Single
