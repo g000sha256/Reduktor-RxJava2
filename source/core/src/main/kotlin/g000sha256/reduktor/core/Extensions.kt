0c647388f68a6a94399a741b69ab1bc25167122d
g000sha256 <g000sha256@gmail.com>
2021-10-14 21:31:37 +0300

    .

--- source/core/src/main/kotlin/g000sha256/reduktor/core/Extensions.kt
+++ source/core/src/main/kotlin/g000sha256/reduktor/core/Extensions.kt
@@ -1,5 +1,16 @@
 package g000sha256.reduktor.core
 
+fun <A, S> Store(
+    initialState: S,
+    reducer: Reducer<A, S>,
+    initializers: List<Initializer<A, S>> = ArrayList(),
+    sideEffects: List<SideEffect<A, S>> = ArrayList(),
+    logger: Logger = Logger {},
+    statesCallback: (state: S) -> Unit
+): Store<A, S> {
+    return Store(logger, reducer, statesCallback, initialState, initializers, sideEffects)
+}
+
 infix fun <A> Actions<A>.post(action: A) {
     post(action)
 }
@@ -22,15 +33,4 @@ operator fun Tasks.plusAssign(task: Task) {
 
 operator fun Tasks.set(key: String, task: Task) {
     add(key, task)
-}
-
-fun <A, S> Store(
-    initialState: S,
-    reducer: Reducer<A, S>,
-    initializers: List<Initializer<A, S>> = ArrayList(),
-    sideEffects: List<SideEffect<A, S>> = ArrayList(),
-    logger: Logger = Logger {},
-    statesCallback: (state: S) -> Unit
-): Store<A, S> {
-    return Store(logger, reducer, statesCallback, initialState, initializers, sideEffects)
 }
